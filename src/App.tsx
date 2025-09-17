4f3664eefab7a342434c1fc451ec0fed9a846a0b
codeLife42 <tailsonandradesb@hotmail.com>
2025-09-17 20:43:32 -0300

    Atualizacao devlinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { YoutubeLogoIcon } from "@phosphor-icons/react";
+import { InstagramLogoIcon, YoutubeLogoIcon } from "@phosphor-icons/react";
 import { LinkedinLogoIcon } from "@phosphor-icons/react";
 import { GithubLogoIcon } from "@phosphor-icons/react";
 import "./App.css";
 import ButtonComponent from "./components/ButtonComponent";
 
+
 export default function App() {
   return (
     <main>
       <header>
-        <img src="https://i.pinimg.com/474x/9f/f4/e2/9ff4e2cde58145983cc045a8b2cae62a.jpg" className="thumb"></img>
-        <h2 className="user-id">@user</h2>
-        <p className="user-description">Descricao do usuario</p>
+        <img src="/profile_picture.jpg" className="thumb" alt="Foto de perfil"></img>
+        <h2 className="user-id">@tailsonandrade</h2>
+        <p className="user-description"><strong>Desenvolvedor web </strong> - Codando hoje para transformar o amanhã.</p>
       </header>
       <nav className="buttons-nav">
-        <ButtonComponent logo={<YoutubeLogoIcon size={40} />} name="Youtube"></ButtonComponent>
-        <ButtonComponent logo={<LinkedinLogoIcon size={40} />} name="LinkedIn"></ButtonComponent>
-        <ButtonComponent logo={<GithubLogoIcon size={40} />} name="GitHub"></ButtonComponent>
+        <ButtonComponent link="https://www.youtube.com/@codelife42" logo={<YoutubeLogoIcon size={40} />} name="Youtube"></ButtonComponent>
+        <ButtonComponent link="https://www.linkedin.com/in/tailson-andrade-961784277" logo={<LinkedinLogoIcon size={40} />} name="LinkedIn"></ButtonComponent>
+        <ButtonComponent link="https://github.com/codeLife42" logo={<GithubLogoIcon size={40} />} name="GitHub"></ButtonComponent>
+        <ButtonComponent link="https://www.instagram.com/tailsonandrade/" logo={<InstagramLogoIcon size={40} />} name="Instagram"></ButtonComponent>
       </nav>
     </main >
   )
--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,19 +1,20 @@
 interface ButtonProps {
     name: string,
-    logo: React.ReactNode;
+    logo: React.ReactNode,
+    link: string
 }
 
 export default function ButtonComponent(props: ButtonProps) {
     return (
         <div>
-            <button>
+            <a className="button-style" href={props.link} target="_blank" rel="noopener noreferrer">
                 <span className="btn-logo">
                     {props.logo}
                 </span>
                 <span className="btn-name">
                     {props.name}
                 </span>
-            </button>
-        </div>
+            </a>
+        </div >
     );
 }
